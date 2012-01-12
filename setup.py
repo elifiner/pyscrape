9ca836cca53deed582213dc383ee22b97f9506da
Eli Golovinsky <elig@il.ibm.com>
2012-01-12 12:18:40 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# Copyright (C) 2011 Eli Golovinsky <eli.golovinsky@gmail.com>.
+#
+# This file is part of Pyscrape.
+#
+# Pyscrape is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# Pyscrape is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pyscrape.  If not, see <http://www.gnu.org/licenses/>.
+
+from distutils.core import setup
+
+DESCRIPTION = """
+Automate and scrape web sites using a friendly pythonic interface. Relies heavily 
+on BeautifulSoup and provides an abstration layer on top of urllib2. Runs equally
+well using urllib2 and on Google App Engine using Google's urlfetch API.
+"""
+
+setup(
+    name='Pyscrape',
+    version='0.1.0',
+    description='A pythonic scraping library for web sites.',
+    long_description=DESCRIPTION,
+    author='Eli Golovinsky',
+    license='MIT',
+    author_email='eli.golovinsky@gmail.com',
+    url='https://github.com/gooli/pyscrape',
+    package_dir={'pyscrape':'.'},
+    packages=['pyscrape'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP'
+    ]
+)
+
